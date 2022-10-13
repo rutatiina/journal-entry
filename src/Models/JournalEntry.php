ff6b67604dd9b4e18297ce77fe890e4c107f896e
Gilbert Rutatiina <rutatiina@gmail.com>
2022-10-13 14:20:33 +0300

    cast canceled column to integer in model

--- src/Models/JournalEntry.php
+++ src/Models/JournalEntry.php
@@ -26,6 +26,10 @@ class JournalEntry extends Model
 
     protected $guarded = [];
 
+    protected $casts = [
+        'canceled' => 'integer',
+    ];
+
     /**
      * The attributes that should be hidden for arrays.
      *
